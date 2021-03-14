df2d78747d00d842147b575d92f6f065775dd648
douglasz1 <douglas.santana.dev@gmail.com>
2021-03-14 08:17:09 -0300

    requests

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const bodyParser = require('body-parser')
 
 
 const app = express()
@@ -14,15 +14,15 @@ app.use(cors())
 
 let db = [
     { '1': { Nome: 'Cliente 1', Idade: '20' } },
-    { '1': { Nome: 'Cliente 1', Idade: '20' } },
-    { '1': { Nome: 'Cliente 1', Idade: '20' } },
+    { '2': { Nome: 'Cliente 1', Idade: '20' } },
+    { '3': { Nome: 'Cliente 1', Idade: '20' } },
 ]
 
 app.get('/', (req, res) => {
     return res.json(db)
-}
+})
 
 
 app.listen(21262, () => {
-    console.log(`Express started at http://localhost:21262`)   
+    console.log(`Express started at http://localhost:21262`)
 })
